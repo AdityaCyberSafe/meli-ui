4fe3fb9f78ac326b8aca27c289ce5fc6fd9f9ae6
Geoffroy Empain <geoffroy@charlie-bravo.be>
2021-01-07 11:16:11 +0100

    fix: typo in add member modal

--- src/components/teams/members/add/AddMember.tsx
+++ src/components/teams/members/add/AddMember.tsx
@@ -79,7 +79,7 @@ export function AddMember({
     <>
       <CardModal
         isOpen={isOpen}
-        title="Delete branch"
+        title="Add member"
         closeModal={() => setIsOpen(false)}
         className={styles.modal}
       >
